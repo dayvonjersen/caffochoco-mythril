2a964c53454e8f2047212518d80f7a184aa95265
tso <tso@teknik.io>
2017-01-06 13:37:41 -0500

    1337

--- server.go
+++ server.go
@@ -1,21 +1,54 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
 	"strings"
 )
 
 func main() {
+	var (
+		addr string
+		port int
+	)
+	flag.StringVar(
+		&addr,
+		"addr",
+		"",
+		"leave blank for 0.0.0.0",
+	)
+	flag.IntVar(
+		&port,
+		"port",
+		8080,
+		"",
+	)
+	flag.Parse()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("->", r.Method, r.URL)
 		file := "index.html"
-		if req := strings.TrimPrefix(r.URL.Path, "/"); fileExists(req) {
+		req := strings.TrimPrefix(r.URL.Path, "/")
+		if fileExists(req) {
 			file = req
+		} else if strings.HasPrefix(req, "bower_components/") {
+			w.WriteHeader(404)
+			log.Println("<- 404 Not Found")
+			io.WriteString(w, "File Not Found")
+			return
 		}
+		log.Println("<- 200 OK")
 		http.ServeFile(w, r, file)
 	})
-	println("Listening on :8000...")
-	println(http.ListenAndServe(":8000", nil))
+
+	listenAddr := fmt.Sprintf("%s:%d", addr, port)
+	log.Println("listening on", listenAddr)
+	log.Fatalln(http.ListenAndServe(listenAddr, nil))
+
 }
 
 func fileExists(filename string) bool {
