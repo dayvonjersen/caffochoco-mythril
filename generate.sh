391f52799e888f623ca89fb65317081482c8f018
tso <tso@teknik.io>
2017-01-06 00:55:42 -0500

    clean up

--- generate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ruby -ryaml -rjson -e 'puts JSON.generate(YAML.load(ARGF))' < data.yaml > data.json
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 "use strict";
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+const gulp   = require('gulp');
+const sass   = require('gulp-sass');
+const exec   = require('child_process').exec;
 
 gulp.task('sass', () => {
     return gulp
@@ -9,8 +10,20 @@ gulp.task('sass', () => {
         .pipe(gulp.dest('.'));
 });
 
+gulp.task('data', (cb) => {
+    exec(
+      "ruby -ryaml -rjson -e 'puts JSON.generate(YAML.load(ARGF))' < data.yaml > data.json",
+      (err, stdout, stderr) => {
+        cb(err);
+        console.log(stdout, stderr);
+      }
+    )
+      .on('exit', (code) => console.log(!code ? 'OK' : 'Fail'));
+});
+
 gulp.task('watch', () => {
     gulp.watch('*.sass', ['sass']);
+    gulp.watch('data.yaml', ['data']);
 });
 
 gulp.task('default', ['watch']);
