a7636c158beaae80f3c87404a00e30c3571751e8
tso <tso@teknik.io>
2017-01-08 14:38:30 -0500

    add vib

--- server.go
+++ server.go
@@ -3,11 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"image"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+
+	_ "image/jpeg"
+	_ "image/png"
+
+	"github.com/generaltso/vibrant"
 )
 
 func main() {
@@ -33,12 +39,22 @@ func main() {
 		log.Println("->", r.Method, r.URL)
 		file := "index.html"
 		req := strings.TrimPrefix(r.URL.Path, "/")
+
 		if fileExists(req) {
 			file = req
 		} else if strings.HasPrefix(req, "bower_components/") {
 			w.WriteHeader(404)
 			log.Println("<- 404 Not Found")
 			io.WriteString(w, "File Not Found")
+			return
+		} else if strings.HasPrefix(req, "image/") && strings.HasSuffix(req, ".css") {
+			if !fileExists(strings.TrimSuffix(req, ".css")) {
+				log.Println("<- 404 Not Found")
+				io.WriteString(w, "File Not Found")
+				return
+			}
+			imageHandler(w, r)
+			log.Println("<- 200 OK")
 			return
 		}
 		log.Println("<- 200 OK")
@@ -49,6 +65,30 @@ func main() {
 	log.Println("listening on", listenAddr)
 	log.Fatalln(http.ListenAndServe(listenAddr, nil))
 
+}
+
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	path := "." + r.URL.Path
+	path = strings.TrimSuffix(path, ".css")
+
+	f, err := os.Open(path)
+	checkErr(err)
+	img, _, err := image.Decode(f)
+	f.Close()
+	checkErr(err)
+	palette, err := vibrant.NewPaletteFromImage(img)
+	checkErr(err)
+	w.Header().Set("Content-Type", "text/css; charset=UTF-8")
+	for _, swatch := range palette.ExtractAwesome() {
+		c := swatch.Color
+		r, g, b := c.RGB()
+		fmt.Fprintf(w, `
+.%s {
+	background-color: %s;
+	color: %s;
+/*	box-shadow: 0 0 10px rgba(%d,%d,%d,1); */
+}`, strings.ToLower(swatch.Name), c, c.TitleTextColor(), r, g, b)
+	}
 }
 
 func fileExists(filename string) bool {
